7438e7c5a70b08f07187e4cb06c1388dd598aa1a
sudokamikaze <pulshencode@outlook.com>
2017-06-13 14:22:02 +0300

    cpufreq: Add bioshock

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -427,6 +427,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
